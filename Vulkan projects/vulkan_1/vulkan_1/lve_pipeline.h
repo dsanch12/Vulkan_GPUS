e6fa4c8ef832398b4b1c1d6653a684afc8798370
dsanch12 <60479613+dsanch12@users.noreply.github.com>
2022-04-29 20:57:09 +0200

    vulkan parte 5

--- Vulkan projects/vulkan_1/vulkan_1/lve_pipeline.h
+++ Vulkan projects/vulkan_1/vulkan_1/lve_pipeline.h
@@ -1,61 +1,62 @@
 #pragma once
 
+#include "lve_device.h"
+
+// std
 #include <string>
 #include <vector>
-#include "lve_device.h"
 
 namespace lve {
 
-	struct PipelineConfigInfo {
+    struct PipelineConfigInfo {
+        PipelineConfigInfo() = default;
+        PipelineConfigInfo(const PipelineConfigInfo&) = delete;
+        PipelineConfigInfo& operator=(const PipelineConfigInfo&) = delete;
 
-		VkViewport viewport;
-		VkRect2D scissor;
-		VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
-		VkPipelineRasterizationStateCreateInfo rasterizationInfo;
-		VkPipelineMultisampleStateCreateInfo multisampleInfo;
-		VkPipelineColorBlendAttachmentState colorBlendAttachment;
-		VkPipelineColorBlendStateCreateInfo colorBlendInfo;
-		VkPipelineDepthStencilStateCreateInfo depthStencilInfo;
-		VkPipelineLayout pipelineLayout = nullptr;
-		VkRenderPass renderPass = nullptr;
-		uint32_t subpass = 0;
-	};
+        VkViewport viewport;
+        VkRect2D scissor;
+        VkPipelineViewportStateCreateInfo viewportInfo;
+        VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
+        VkPipelineRasterizationStateCreateInfo rasterizationInfo;
+        VkPipelineMultisampleStateCreateInfo multisampleInfo;
+        VkPipelineColorBlendAttachmentState colorBlendAttachment;
+        VkPipelineColorBlendStateCreateInfo colorBlendInfo;
+        VkPipelineDepthStencilStateCreateInfo depthStencilInfo;
+        VkPipelineLayout pipelineLayout = nullptr;
+        VkRenderPass renderPass = nullptr;
+        uint32_t subpass = 0;
+    };
 
-	class LvePipeline {
+    class LvePipeline {
+    public:
+        LvePipeline(
+            LveDevice& device,
+            const std::string& vertFilepath,
+            const std::string& fragFilepath,
+            const PipelineConfigInfo& configInfo);
+        ~LvePipeline();
 
-	public:
+        LvePipeline(const LvePipeline&) = delete;
+        void operator=(const LvePipeline&) = delete;
 
-		LvePipeline(
-			LveDevice& device,
-			const std::string& vertFilepath,
-			const std::string& fragFilepath,
-			const PipelineConfigInfo& configInfo
-		);
-	
-		~LvePipeline();
+        void bind(VkCommandBuffer commandBuffer);
 
-		LvePipeline(const LveDevice&) = delete;
-		void operator=(const LvePipeline&) = delete;
+        static void defaultPipelineConfigInfo(
+            PipelineConfigInfo& configInfo, uint32_t width, uint32_t height);
 
-		void bind(VkCommandBuffer commandBuffer);
+    private:
+        static std::vector<char> readFile(const std::string& filepath);
 
-		static PipelineConfigInfo defaultPipelineConfigInfo(uint32_t width, uint32_t height);
+        void createGraphicsPipeline(
+            const std::string& vertFilepath,
+            const std::string& fragFilepath,
+            const PipelineConfigInfo& configInfo);
 
-	private:
+        void createShaderModule(const std::vector<char>& code, VkShaderModule* shaderModule);
 
-		static std::vector<char> readFile(const std::string& fragFilepath);
-
-		void createGraphicsPipeline(
-			const std::string& vertFilepath,
-			const std::string& fragFilepath,
-			const PipelineConfigInfo& configInfo
-		);
-
-		void createShaderModule(const std::vector<char>& code, VkShaderModule* shaderModule);
-
-		LveDevice& lveDevice;
-		VkPipeline graphicsPipeline;
-		VkShaderModule vertShaderModule;
-		VkShaderModule fragShaderModule;
-	};
+        LveDevice& lveDevice;
+        VkPipeline graphicsPipeline;
+        VkShaderModule vertShaderModule;
+        VkShaderModule fragShaderModule;
+    };
 }
--- Vulkan projects/vulkan_1/vulkan_1/v1_app.cpp
+++ Vulkan projects/vulkan_1/vulkan_1/v1_app.cpp
@@ -1,128 +1,114 @@
 #include "v1_app.h"
-#include <stdexcept>
+
+// std
 #include <array>
+#include <stdexcept>
 
 namespace lve {
 
-	App::App() {
-	
-		createPipelineLayout();
-		createPipeline();
-		createCommandBuffers();
-	}
+    App::App() {
+        createPipelineLayout();
+        createPipeline();
+        createCommandBuffers();
+    }
 
-	App::~App() {
-		vkDestroyPipelineLayout(lveDevice.device(), pipelineLayout, nullptr);
-	}
+    App::~App() { vkDestroyPipelineLayout(lveDevice.device(), pipelineLayout, nullptr); }
 
-	void App::run() {
-	
-		while (!lveWindow.shouldClose()) {
-		
-			glfwPollEvents();
-			drawFrame();
-		}
-	}
+    void App::run() {
+        while (!lveWindow.shouldClose()) {
+            glfwPollEvents();
+            drawFrame();
+        }
 
-	void App::createPipelineLayout() {
-	
-		VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
-		pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
-		pipelineLayoutInfo.setLayoutCount = 0;
-		pipelineLayoutInfo.pSetLayouts = nullptr;
-		pipelineLayoutInfo.pushConstantRangeCount = 0;
-		pipelineLayoutInfo.pPushConstantRanges = nullptr;
+        vkDeviceWaitIdle(lveDevice.device());
+    }
 
-		if (vkCreatePipelineLayout(lveDevice.device(), &pipelineLayoutInfo, nullptr, &pipelineLayout) != VK_SUCCESS) {
-			
-			throw std::runtime_error("failed to create pipeline layout");
-		}
-	}
+    void App::createPipelineLayout() {
+        VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
+        pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+        pipelineLayoutInfo.setLayoutCount = 0;
+        pipelineLayoutInfo.pSetLayouts = nullptr;
+        pipelineLayoutInfo.pushConstantRangeCount = 0;
+        pipelineLayoutInfo.pPushConstantRanges = nullptr;
+        if (vkCreatePipelineLayout(lveDevice.device(), &pipelineLayoutInfo, nullptr, &pipelineLayout) !=
+            VK_SUCCESS) {
+            throw std::runtime_error("failed to create pipeline layout!");
+        }
+    }
 
-	void App::createPipeline() {
-	
-		auto pipelineConfig = LvePipeline::defaultPipelineConfigInfo(lveSwapChain.width(), lveSwapChain.height());
-	
-		pipelineConfig.renderPass = lveSwapChain.getRenderPass();
-		pipelineConfig.pipelineLayout = pipelineLayout;
-		lvePipeline = std::make_unique<LvePipeline>(
-			lveDevice,
-			"shaders/simple_shader.vert.spv",
-			"shaders/simple_shader.frag.spv",
-			pipelineConfig);
-	}
+    void App::createPipeline() {
+        PipelineConfigInfo pipelineConfig{};
+        LvePipeline::defaultPipelineConfigInfo(
+            pipelineConfig,
+            lveSwapChain.width(),
+            lveSwapChain.height());
+        pipelineConfig.renderPass = lveSwapChain.getRenderPass();
+        pipelineConfig.pipelineLayout = pipelineLayout;
+        lvePipeline = std::make_unique<LvePipeline>(
+            lveDevice,
+            "shaders/simple_shader.vert.spv",
+            "shaders/simple_shader.frag.spv",
+            pipelineConfig);
+    }
 
-	void App::createCommandBuffers() {
-	
-		commandBuffers.resize(lveSwapChain.imageCount());
+    void App::createCommandBuffers() {
+        commandBuffers.resize(lveSwapChain.imageCount());
 
-		VkCommandBufferAllocateInfo allocInfo{};
+        VkCommandBufferAllocateInfo allocInfo{};
+        allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+        allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+        allocInfo.commandPool = lveDevice.getCommandPool();
+        allocInfo.commandBufferCount = static_cast<uint32_t>(commandBuffers.size());
 
-		allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
-		allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
-		allocInfo.commandPool = lveDevice.getCommandPool();
-		allocInfo.commandBufferCount = static_cast<uint32_t>(commandBuffers.size());
-	
-		if (vkAllocateCommandBuffers(lveDevice.device(), &allocInfo, commandBuffers.data()) != VK_SUCCESS) {
+        if (vkAllocateCommandBuffers(lveDevice.device(), &allocInfo, commandBuffers.data()) !=
+            VK_SUCCESS) {
+            throw std::runtime_error("failed to allocate command buffers!");
+        }
 
-			throw std::runtime_error("failed to create command buffers");
-		}
+        for (int i = 0; i < commandBuffers.size(); i++) {
+            VkCommandBufferBeginInfo beginInfo{};
+            beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
 
-		for (int i = 0; i < commandBuffers.size(); i++) {
+            if (vkBeginCommandBuffer(commandBuffers[i], &beginInfo) != VK_SUCCESS) {
+                throw std::runtime_error("failed to begin recording command buffer!");
+            }
 
-			VkCommandBufferBeginInfo beginInfo{};
-			beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+            VkRenderPassBeginInfo renderPassInfo{};
+            renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+            renderPassInfo.renderPass = lveSwapChain.getRenderPass();
+            renderPassInfo.framebuffer = lveSwapChain.getFrameBuffer(i);
 
-			if (vkBeginCommandBuffer(commandBuffers[i], &beginInfo) != VK_SUCCESS) {
+            renderPassInfo.renderArea.offset = { 0, 0 };
+            renderPassInfo.renderArea.extent = lveSwapChain.getSwapChainExtent();
 
-				throw std::runtime_error("failed to begin recording");
-			}
+            std::array<VkClearValue, 2> clearValues{};
+            clearValues[0].color = { 0.1f, 0.1f, 0.1f, 1.0f };
+            clearValues[1].depthStencil = { 1.0f, 0 };
+            renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
+            renderPassInfo.pClearValues = clearValues.data();
 
-			VkRenderPassBeginInfo renderPassInfo{};
-			renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
-			renderPassInfo.renderPass = lveSwapChain.getRenderPass();
-			renderPassInfo.framebuffer = lveSwapChain.getFrameBuffer(i);
+            vkCmdBeginRenderPass(commandBuffers[i], &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
 
-			renderPassInfo.renderArea.offset = { 0, 0 };
-			renderPassInfo.renderArea.extent = lveSwapChain.getSwapChainExtent();
+            lvePipeline->bind(commandBuffers[i]);
+            vkCmdDraw(commandBuffers[i], 3, 1, 0, 0);
 
-			std::array<VkClearValue, 2> clearValues{};
-			clearValues[0].color = {0.1f, 0.1f, 0.1f, 0.1f};
-			clearValues[1].depthStencil = {1.0f, 0};
-			renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
-			renderPassInfo.pClearValues = clearValues.data();
+            vkCmdEndRenderPass(commandBuffers[i]);
+            if (vkEndCommandBuffer(commandBuffers[i]) != VK_SUCCESS) {
+                throw std::runtime_error("failed to record command buffer!");
+            }
+        }
+    }
+    void App::drawFrame() {
+        uint32_t imageIndex;
+        auto result = lveSwapChain.acquireNextImage(&imageIndex);
+        if (result != VK_SUCCESS && result != VK_SUBOPTIMAL_KHR) {
+            throw std::runtime_error("failed to acquire swap chain image!");
+        }
 
-			vkCmdBeginRenderPass(commandBuffers[i], &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
-		
-			lvePipeline->bind(commandBuffers[i]);
-			vkCmdDraw(commandBuffers[i], 3, 1, 0, 0);
+        result = lveSwapChain.submitCommandBuffers(&commandBuffers[imageIndex], &imageIndex);
+        if (result != VK_SUCCESS) {
+            throw std::runtime_error("failed to present swap chain image!");
+        }
+    }
 
-			vkCmdEndRenderPass(commandBuffers[i]);
-
-			if (vkEndCommandBuffer(commandBuffers[i]) != VK_SUCCESS) {
-
-				throw std::runtime_error("failed to record command buffer");
-			}
-		}
-	}
-
-	void App::drawFrame() {
-	
-		uint32_t imageIndex;
-		auto result = lveSwapChain.acquireNextImage(&imageIndex);
-	
-		if (result != VK_SUCCESS && result != VK_SUBOPTIMAL_KHR) {
-		
-			throw std::runtime_error("failed to acquire swap chain image");
-		}
-
-		result = lveSwapChain.submitCommandBuffers(&commandBuffers[imageIndex], &imageIndex);
-
-		if (result != VK_SUCCESS) {
-
-			throw std::runtime_error("failed to present swap chain image");
-		}
-
-	
-	}
 }
