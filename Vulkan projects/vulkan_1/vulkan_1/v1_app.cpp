4163e2a0f39c0c43045183d496d99ff81d25f20c
dsanch12 <60479613+dsanch12@users.noreply.github.com>
2022-04-29 19:12:18 +0200

    v5

--- Vulkan projects/vulkan_1/vulkan_1/v1_app.cpp
+++ Vulkan projects/vulkan_1/vulkan_1/v1_app.cpp
@@ -1,6 +1,18 @@
 #include "v1_app.h"
+#include <stdexcept>
 
 namespace lve {
+
+	App::App() {
+	
+		createPipelineLayout();
+		createPipeline();
+		createCommandBuffers();
+	}
+
+	App::~App() {
+		vkDestroyPipelineLayout(lveDevice.device(), pipelineLayout, nullptr);
+	}
 
 	void App::run() {
 	
@@ -9,4 +21,35 @@ namespace lve {
 			glfwPollEvents();
 		}
 	}
+
+	void App::createPipelineLayout() {
+	
+		VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
+		pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+		pipelineLayoutInfo.setLayoutCount = 0;
+		pipelineLayoutInfo.pSetLayouts = nullptr;
+		pipelineLayoutInfo.pushConstantRangeCount = 0;
+		pipelineLayoutInfo.pPushConstantRanges = nullptr;
+
+		if (vkCreatePipelineLayout(lveDevice.device(), &pipelineLayoutInfo, nullptr, &pipelineLayout) != VK_SUCCESS) {
+			
+			throw std::runtime_error("failed to create pipeline layout");
+		}
+	}
+
+	void App::createPipeline() {
+	
+		auto pipelineConfig = LvePipeline::defaultPipelineConfigInfo(lveSwapChain.width(), lveSwapChain.height());
+	
+		pipelineConfig.renderPass = lveSwapChain.getRenderPass();
+		pipelineConfig.pipelineLayout = pipelineLayout;
+		lvePipeline = std::make_unique<LvePipeline>(
+			lveDevice,
+			"shaders/simple_shader.vert.spv",
+			"shaders/simple_shader.frag.spv",
+			pipelineConfig);
+	}
+
+	void App::createCommandBuffers() {}
+	void App::drawFrame() {}
 }
--- Vulkan projects/vulkan_1/vulkan_1/v1_app.h
+++ Vulkan projects/vulkan_1/vulkan_1/v1_app.h
@@ -1,7 +1,12 @@
 #pragma once
 
-#include "v1.h"
+#include "lve_device.h"
 #include "lve_pipeline.h"
+#include "lve_swap_chain.h"
+#include "v1_window.h"
+
+#include <memory>
+#include <vector>
 
 namespace lve {
 
@@ -12,13 +17,26 @@ namespace lve {
 			static constexpr int WIDTH = 500;
 			static constexpr int HEIGHT = 500;
 
+			App();
+			~App();
+
+			App(const App&) = delete;
+			App& operator=(const App&) = delete;
+
 			void run();
 
 		private:
 
+			void createPipelineLayout();
+			void createPipeline();
+			void createCommandBuffers();
+			void drawFrame();
+
 			LveWindow lveWindow{ WIDTH, HEIGHT, "Vulkan_window" };
-
-			LvePipeline lvePipeline{"shaders/simple_shader.vert.spv", "shaders/simple_shader.frag.spv" };
-
+			LveDevice lveDevice{ lveWindow };
+			LveSwapChain lveSwapChain{ lveDevice, lveWindow.getExtent()};
+			std::unique_ptr<LvePipeline> lvePipeline;
+			VkPipelineLayout pipelineLayout;
+			std::vector<VkCommandBuffer> commandBuffers;
 	};
 }
