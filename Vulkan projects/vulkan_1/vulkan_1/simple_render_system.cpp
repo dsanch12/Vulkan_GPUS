0f351932179d0434aa11ae7e7f9fa7562d7c9cb6
dsanch12 <60479613+dsanch12@users.noreply.github.com>
2022-05-02 17:15:03 +0200

    v.13

--- Vulkan projects/vulkan_1/vulkan_1/simple_render_system.cpp
+++ Vulkan projects/vulkan_1/vulkan_1/simple_render_system.cpp
@@ -61,7 +61,9 @@ namespace lve {
     }
 
     void SimpleRenderSystem::renderGameObjects(
-        VkCommandBuffer commandBuffer, std::vector<LveGameObject>& gameObjects) {
+        VkCommandBuffer commandBuffer,
+        std::vector<LveGameObject>& gameObjects,
+        const LveCamera& camera) {
         lvePipeline->bind(commandBuffer);
 
         for (auto& obj : gameObjects) {
@@ -71,7 +73,7 @@ namespace lve {
             SimplePushConstantData push{};
             
             push.color = obj.color;
-            push.transform = obj.transform.mat4();
+            push.transform = camera.getProjection() * obj.transform.mat4();
 
             vkCmdPushConstants(
                 commandBuffer,
